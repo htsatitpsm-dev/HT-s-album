3236ed84488f1fc947686219f5c51475bf62f0c4
htsatitpsm-dev <ht.satitpsm@gmail.com>
2026-07-08 23:52:53 +0700

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,104 @@
+const LANG = {
+    th: {
+        head: "HT's Album",
+        des: "Health Tech • วิทยาศาสตร์ • การศึกษาดูงาน • การแข่งขัน • โครงการโอลิมปิกวิชาการ",
+        level6: "มัธยมศึกษาปีที่ 6",
+        level5: "มัธยมศึกษาปีที่ 5",
+        level4: "มัธยมศึกษาปีที่ 4",
+        posn: "ผลงาน สอวน",
+        com: "ผลงานแข่งขัน",
+        obs: "✦ศึกษาดูงาน",
+        detail: "ดูรายละเอียด",
+        drone1: "หนูน้อยจ้าวเวหา",
+        drone2: "หนูน้อยเจ้าเวหา 2",
+        agri: "แนวคิดนวัตกรรมการเกษตรเพื่ออนาคต",
+        dental1: "ศึกษาดูงานคณะทันตแพทย์มหิดลฯ&จุฬาฯ",
+        dental2: "Workshop ทันตกรรม",
+        sensor: "Work shop การปฎิบัติการใช้งาน Arduino และ Sensorทางวิทยาศาสตร์",
+        VDO: "Workshop “Design Thinking + เทคนิคการตัดต่อ VDO” ",
+        mind: "เข้าเยี่ยมชม ศูนย์พัฒนานวัตกรรมทางการแพทย์(MIND Center) คณะแพทย์ศาสตร์ โรงพยาบาลรามาธิบดี",
+        vet: "เข้าเยี่ยมชม พิพิธภัณฑ์ กายวิภาคศาสตร์ทางสัตวแพทย์ คณะสัตวแพทย์ศาสตร์มหาวิทยาลัยเกษตรศาสตร์",
+        airline: "เข้าเยี่ยมชม บริษัทการบินไทยจํากัด (มหาชน)",
+        dental: "เข้าเยี่ยมชม Dr.Sunil International Dental Center",
+        dek: "HACKATHON เด็กไทยแข็งแรง คนไทยแข็งแรง “START YOUNG, STAY STRONG”",
+        drone3: "หนูน้อยเจ้าเวหา 3",
+        futurium: "นิทรรศการ Innovation World Futurium",
+        med: "Work shop คณะแพทย์ศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย",
+        anatomy: "Work shop Exploring 3D Anatomy Print คณะแพทย์ศาสตร์ มหาวิทยาลัยศรีนครินทรวิโรฒ",
+        med1: "เข้าเยี่ยมชมห้องเรียนอาจารย์ใหญ่ คณะแพทย์ศาสตร์ มหาวิทยาลัยศรีนครินทรวิโรฒ",
+        med2: "เข้าเยี่ยมชมห้องเรียนศูนย์ SIMS (Simulation)งานแพทยศาสตร์ศึกษา คณะแพทยศาสตร์ มศว องครักษ์ ",
+        astro: "เข้าเยี่ยมชมสถาบันวิจัยดาราศาสตร์แห่งชาติ อุทยานดาราศาสตร์สิรินธร (Princess Sirindhorn AstroPark) จ.เชียงใหม่",
+        story: "“พี่นิสิตเเพทย์เล่าให้น้องฟัง”",
+        model: "Work shop การสร้างโมเดล 3Dช่องปาก ด้วยการสแกน",
+        coding: "การอบรม Coding Workshop: Data Analysis of Black Holes ณ มหาวิทยาลัยเทคโนโลยีสุรนารี",
+        biosensor: "ไบโอเซนเซอร์สำหรับนวัตกรรมทางการแพทย์และแนวทางการสร้างนวัตกรรมไบโอเซนเซอร์",
+        bioeng: "Work shop ทางด้านวิศวกรรมชีวการแพทย์",
+        draw: "Work shop  วาดรูปทางด้านชีววิทยา และการทําAR",
+        app: "Workshop หัวข้อ “แนวทางการพัฒนานวัตกรรมแอพลิเคชันทางการแพทย์”",
+        major: "การจัดการเรียนการสอน วิชาเอก",
+        dtmed: "✦แนวคิดนวัตกรรมทางการแพทย์",
+        obsci: "✦สัมนาทางวิทยาศาสตร์",
+        bsinno: "✦พื้นฐานนวัตกรรม",
+    },
+    en: {
+        head: "HT's Album",
+        des: "Health Tech • Science • Observation • Competition • POSN",
+        level6: "Grade 12",
+        level5: "Grade 11",
+        level4: "Grade 10",
+        obs: "✦Observation",
+        com: "Competition",
+        posn: "POSN ",
+        major: "Major Subject Instruction",
+        detail: "Detail",
+        drone1: "Drone Mission",
+        drone2: "Drone Mission 2",
+        agri: "Innovative Agricultural Concepts for the Future",
+        dental1: "Observation at Faculty of Dentistry, Mahidol University & Chulalongkorn University",
+        dental2: "Dental Workshop",
+        sensor: "Workshop on Using Arduino and Science Sensors",
+        VDO: "Workshop “Design Thinking + VDO Editing Techniques” ",
+        mind: "Visit to the Medical Innovation Center (MIND Center), Faculty of Medicine, Ramathibodi Hospital",
+        vet: "Visit to the Veterinary Anatomy Museum, Faculty of Veterinary Science, Kasetsart University",
+        airline: "Visit to Thai Airways International (Public Company) Limited",
+        dental: "Visit to Dr.Sunil International Dental Center",
+        dek: "HACKATHON “START YOUNG, STAY STRONG”",
+        drone3: "Drone Mission 3",
+        futurium: "Innovation World Futurium Exhibition",
+        med: "Visit to the SIMS (Simulation) Center, Medical Education, Faculty of Medicine, Srinakharinwirot University Ongkharak Campus",
+        anatomy: "Workshop Exploring 3D Anatomy Print, Faculty of Medicine, Srinakharinwirot University",
+        med1: "Visit to the Main Lecture Hall, Faculty of Medicine, Srinakharinwirot University",
+        med2: "Visit to the SIMS (Simulation) Center, Medical Education, Faculty of Medicine, Srinakharinwirot University Ongkharak Campus",
+        astro: "Visit to the National Astronomical Research Institute of Thailand, Princess Sirindhorn AstroPark, Chiang Mai Province",
+        story: "“Senior Medical Students Share Their Stories with Juniors”",
+        model: "Workshop on Creating 3D Oral Models Using Scanning",
+        coding: "Coding Workshop: Data Analysis of Black Holes at Suranaree University of Technology",
+        biosensor: "Biosensors for Medical Innovation and Guidelines for Creating Biosensor Innovations",
+        bioeng: "Biomedical Engineering Workshop",
+        draw: "Workshop on Biological Drawing and AR Creation",
+        app: "Workshop on “Guidelines for Developing Medical Application Innovations”",
+        dtmed: "✦Medical Innovation Concepts",
+        obsci: "✦Scientific Seminars",
+        bsinno: "✦Fundamentals of Innovation",
+    }
+};
+
+function setLang(lang) {
+    localStorage.setItem("lang", lang);
+    applyLang();
+}
+
+
+function applyLang() {
+    const lang = localStorage.getItem("lang") || "th";
+
+    document.querySelectorAll("[data-lang]").forEach(el => {
+        const key = el.getAttribute("data-lang");
+        if (LANG[lang][key]) {
+            el.innerText = LANG[lang][key];
+        }
+    });
+}
+
+
+window.onload = applyLang;
